b7eecb32fc1618c45fe1eae3cde08cc2d5c62724
ravichander34 <retikyala@gmail.com>
2023-10-10 21:44:48 +0530

    Add files via upload

---
+++ testApp.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import os
+# from langchain.agents import create_csv_agent
+# from langchain.llms import OpenAI
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = "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"
+
+# Read the CSV file
+df = pd.read_csv('Electric_Production.csv')
+
+st.title('File Interpreter App')
+
+#upload file tab 
+uploaded_file = st.file_uploader("Upload a file", type=["csv", "txt", "xlsx","pdf"])
+
+if uploaded_file is not None:
+    st.write(" Uploaded format is " + uploaded_file.type)
+    if "csv" in uploaded_file.type :
+        st.write(" Uploaded file is csv ")
+        data = uploaded_file.read()
+        df = pd.read_csv(uploaded_file.name)
+        st.write(" Error is here")
+        st.write(df.head())
+
+#create an agent
+# agent = create_csv_agent(OpenAI(temperature=0), uploaded_file.name , verbose=True, max_iterations=30)
+
+
+
+# if uploaded_file is not None:
+#     # Display some information about the uploaded file
+#     st.write("File details:")
+#     st.write(f"Filename: {uploaded_file.name}")
+#     st.write(f"File type: {uploaded_file.type}")
+#     st.write(f"File size (bytes): {uploaded_file.size}")
+
+#     # You can also read and display the contents of the file
+#     st.write("File contents:")
+    
+#     st.write(data)
